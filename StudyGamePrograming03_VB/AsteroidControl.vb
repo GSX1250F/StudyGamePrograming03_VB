9edde0780b5c5095ec89a94608f339b8f9556984
tanada@sandagakuen.ed.jp <tanada@sandagakuen.ed.jp>
2024-07-20 20:41:03 +0900

    ccc

--- StudyGamePrograming03_VB/AsteroidControl.vb
+++ StudyGamePrograming03_VB/AsteroidControl.vb
@@ -2,10 +2,12 @@
     Inherits Actor
 
     Private mAsteroidCooldown As Double      '小惑星増殖までの待機時間
+    Private InitCooldown As Double      '小惑星増殖までの待機時間
 
     Public Sub New(ByRef game As Game)
         MyBase.New(game)
-        mAsteroidCooldown = 4.0
+        InitCooldown = 5.0
+        mAsteroidCooldown = InitCooldown
     End Sub
     Public Overrides Sub UpdateActor(ByVal deltaTime As Double)
         mAsteroidCooldown -= deltaTime
@@ -13,7 +15,7 @@
         Dim numAsteroids As Integer = GetGame().GetAsteroids().Count()
         If (mAsteroidCooldown < 0.0 And numAsteroids > 0) Then
             GetGame().AddAsteroid()
-            mAsteroidCooldown = 5.0
+            mAsteroidCooldown = InitCooldown
         End If
     End Sub
 End Class
